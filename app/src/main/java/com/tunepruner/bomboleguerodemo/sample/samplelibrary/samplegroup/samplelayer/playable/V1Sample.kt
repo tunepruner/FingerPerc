043af21fcbde11c901d529d40053de21872a4f60
Philip Carlson <writephilcarlson@gmail.com>
2021-02-04 16:10:08 -0500

    Gets SoundPool working.
    Much better with delay, last longer than with MediaPlayer, but still runs out of resources somehow

--- app/src/main/java/com/tunepruner/bomboleguerodemo/sample/samplelibrary/samplegroup/samplelayer/playable/V1Sample.kt
+++ app/src/main/java/com/tunepruner/bomboleguerodemo/sample/samplelibrary/samplegroup/samplelayer/playable/V1Sample.kt
@@ -1,9 +1,12 @@
 package com.tunepruner.bomboleguerodemo.sample.samplelibrary.samplegroup.samplelayer.playable
 
 import android.content.res.AssetFileDescriptor
+import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.SoundPool
+import android.os.Build
 import android.util.Log
-import com.tunepruner.bomboleguerodemo.sample.samplelibrary.samplegroup.samplelayer.RoundRobinLogic
+import androidx.annotation.RequiresApi
 import java.util.concurrent.ConcurrentLinkedQueue
 
 
@@ -12,53 +15,28 @@ class V1Sample(
     private val assetFileDescriptor: AssetFileDescriptor,
 ) :
     Playable {
-    private val availPlayers = ConcurrentLinkedQueue<MediaPlayer>()
-    private val busyPlayers = ConcurrentLinkedQueue<MediaPlayer>()
-
-    //Or <Exoplayer> later?
-
-    init {
-        while (availPlayers.size < 1) {
-            createAnotherPlayer()
-        }
-        Log.i("sizeOfAvail", "${availPlayers.size}")
-    }
-
-    private fun createAnotherPlayer() {
-        var mediaPlayer = MediaPlayer()
-        mediaPlayer.setDataSource(
-            assetFileDescriptor.fileDescriptor,
-            assetFileDescriptor.startOffset,
-            assetFileDescriptor.length
-        );
-        mediaPlayer.prepare()
-        availPlayers.add(mediaPlayer)
-
-    }
-
+    val audioAttributes = AudioAttributes.Builder()
+        .setUsage(
+            AudioAttributes.USAGE_ASSISTANCE_SONIFICATION
+        )
+        .setContentType(
+            AudioAttributes.CONTENT_TYPE_SONIFICATION
+        )
+        .build()
+    val soundPool = SoundPool.Builder()
+        .setMaxStreams(3)
+        .setAudioAttributes(
+            audioAttributes
+        )
+        .build()
+    var soundID = soundPool.load(assetFileDescriptor, 1)
 
     override fun play() {
-        var player = availPlayers.last()
-        player.setOnCompletionListener {
-            busyPlayers.remove(it)
-            availPlayers.add(it)
-        }
-
-
-        if (availPlayers.size > 0) {
-            availPlayers.remove(player)
-            busyPlayers.add(player)
-            player.start()
-        } else {
-            createAnotherPlayer()
-            play()
-        }
+        soundPool.play(soundID, 1F, 1F, 1, 0, 1F)
     }
 
 
     override fun getSampleCoords(): SampleCoords {
         return sampleCoords
     }
-
-    private fun moveToBusy() {}
 }
